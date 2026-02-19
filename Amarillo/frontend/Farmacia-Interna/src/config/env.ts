8fa98881aafedd8c2ebacee6d54a9f1b858c43a3
Lunar-Luis <luislunar54321@gmail.com>
2026-02-19 01:01:03 -0400

    Update Cloudflare URL

--- Amarillo/frontend/Farmacia-Interna/src/config/env.ts
+++ Amarillo/frontend/Farmacia-Interna/src/config/env.ts
@@ -1,4 +1,4 @@
 // Archivo: src/config/env.ts
 //export const API_URL = "http://localhost:8080/api";
 
-export const API_URL = "https://poker-edgar-remark-recovered.trycloudflare.com/api";
+export const API_URL = " https://where-cited-hash-involvement.trycloudflare.com/api";
