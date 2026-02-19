16d09d435997531aff24a396505079b6c4852830
Lunar-Luis <luislunar54321@gmail.com>
2026-02-19 12:40:21 -0400

    Update url V1.1

--- Amarillo/frontend/Farmacia-Interna/src/config/env.ts
+++ Amarillo/frontend/Farmacia-Interna/src/config/env.ts
@@ -1,4 +1,4 @@
 // Archivo: src/config/env.ts
 //export const API_URL = "http://localhost:8080/api";
 
-export const API_URL = "  https://sys-dover-billing-illustrations.trycloudflare.com/api";
+export const API_URL = " https://zones-struct-unto-bool.trycloudflare.com/api";
