b046210c988548f55dad974e01d7cfb1810cad72
Lunar-Luis <luislunar54321@gmail.com>
2026-02-19 00:19:17 -0400

    Actualizar API URL a Cloudflare Tunnel

--- Amarillo/frontend/Farmacia-Interna/src/config/env.ts
+++ Amarillo/frontend/Farmacia-Interna/src/config/env.ts
@@ -1,2 +1,4 @@
 // Archivo: src/config/env.ts
-export const API_URL = "http://localhost:8080/api";
+//export const API_URL = "http://localhost:8080/api";
+
+export const API_URL = "https://poker-edgar-remark-recovered.trycloudflare.com/api";
